0963e181745da090754c14cf7ba6dc8cc27eecef
HenricoBirochi <henrico.birochi@gmail.com>
2025-09-26 14:31:45 -0300

    feat: Two css files added and new method on views.py

--- meetups/views.py
+++ meetups/views.py
@@ -4,11 +4,20 @@ from django.shortcuts import render
 
 def index(request):
     meetups = [
-        {'title': 'A First Meetup'},
-        {'title': 'A Seconde Meetup'}
+        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
+        {'title': 'A Seconde Meetup', 'location': 'Paris','slug': 'a-second-meetup'}
     ]
-
     return render(request, 'meetups/index.html', {
         'show_meetups': False,
         'meetups': meetups
+    })
+
+def meetup__details(request):
+    selected_meetup = {
+        'title': 'A First Meetup', 
+        'description': 'This is the first meetup!'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
     })
