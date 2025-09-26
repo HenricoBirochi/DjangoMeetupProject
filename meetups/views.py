2c4deacde93f39546b34e574bcb723e0511ac803
HenricoBirochi <henrico.birochi@gmail.com>
2025-09-26 10:52:55 -0300

    feat: Base html and css files added, settings changed to support this files and some logic in the meetups views.py file

--- meetups/views.py
+++ meetups/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
-    
+    meetups = [
+        {'title': 'A First Meetup'},
+        {'title': 'A Seconde Meetup'}
+    ]
+
+    return render(request, 'meetups/index.html', {
+        'show_meetups': False,
+        'meetups': meetups
+    })
