f47686d415f9e61e1e3822b81be6730177f517bd
henrico <henrico.birochi@gmail.com>
2025-09-26 23:08:20 -0300

    feat: Some changes in the views.py and in the templates to receive the new table on models.py

--- meetups/views.py
+++ meetups/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
 
+from .models import Meetup
+
 # Create your views here.
 
 def index(request):
-    meetups = [
-        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
-        {'title': 'A Seconde Meetup', 'location': 'Paris','slug': 'a-second-meetup'}
-    ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
-        'show_meetups': False,
         'meetups': meetups
     })
 
 def meetup__details(request, meetup_slug):
-    selected_meetup = {
-        'title': 'A First Meetup', 
-        'description': 'This is the first meetup!'
-    }
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_slug': meetup_slug,
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description']
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description
+        })
+    except Exception as exception:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False
+        })
