37d7bf130109aa5671bfcc4f789b7163da530296
HenricoBirochi <henrico.birochi@gmail.com>
2025-09-29 13:56:55 -0300

    feat: Participant table added, page of registration success added, some changes to receive the new table on the project

--- meetups/models.py
+++ meetups/models.py
@@ -8,13 +8,22 @@ class Location(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.address})'
+    
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
 
 class Meetup(models.Model):
-    title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True)
 
     def __str__(self):
         return self.title
